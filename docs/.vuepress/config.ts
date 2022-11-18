4556764ea03d3362bd41de9ae0075818229aa242
wecle <837968732@qq.com>
2022-11-18 15:11:32 +0800

    init press

---
+++ docs/.vuepress/config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vuepress/config'
+
+export default defineConfig({
+    title: 'PilibiliPress',
+    description: 'A Wonderful Pilibili Press',
+    themeConfig: {
+        repo: "Wecle/pilibili-press",
+        sidebar: [
+            {
+                title: "起步",
+                path: '/'
+            }
+        ]
+    }
+})
